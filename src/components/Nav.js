adf2806641012008c7eef6966e3736495358c383
Shahzaib Rao <30550027+HexCode29@users.noreply.github.com>
2020-10-19 19:12:14 +0500

    make links dynamic

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,12 +3,13 @@ import "react-bootstrap";
 import {Nav} from 'react-bootstrap';
 // import styled from  "styled-components";
 import "../nav.css";
+import {Link} from "react-router-dom";
 
 export default function Navigation(){
     return(
         <div id="nav">            
              <Nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-                <a class="navbar-brand" href="/">Al-Madni Motors</a>
+                <Link class="navbar-brand" href="/">Al-Madni Motors</Link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -16,16 +17,16 @@ export default function Navigation(){
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                        <Link class="nav-link" href="/">Home <span class="sr-only">(current)</span></Link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/gallery">Gallery</a>
+                        <Link class="nav-link" href="/gallery">Gallery</Link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="/about">About</a>
+                        <Link class="nav-link disabled" href="/about">About</Link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="/contact">Contact</a>
+                        <Link class="nav-link disabled" href="/contact">Contact</Link>
                     </li>
                     </ul>
                 </div>
